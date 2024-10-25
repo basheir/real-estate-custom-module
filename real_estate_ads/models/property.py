ef212a4f37f309f54789fba9ef9389477a0130ca
Bashir MAhad <122716610+bashiirmahad@users.noreply.github.com>
2024-10-25 08:45:38 +0300

    Property model added

--- real_estate_ads/models/property.py
+++ real_estate_ads/models/property.py
@@ -9,3 +9,12 @@ class Property(models.Model):
     date_availability = field.Date(string="Available From")
     expected_price = field.Float(string="Expected Price")
     selling_price = field.FLoad(string="Selling Price")
+    bedrooms = field.Integer(string="Bedrooms")
+    living_area = field.Integer(string="Living Area(sqm)")
+    facades = field.Integer(string="Facades")
+    garage = field.Boolean(string="Garage", default=False)
+    garden = field.Boolean(string="Garden", default=False)
+    garden_area = field.Integer(string="Garden area")
+    garden_orientation = field.selection([('north', 'North'), ('south', 'south'), ('east', 'East'), ('west', 'West')], string="Garden Orientation", default='north')
+
+
